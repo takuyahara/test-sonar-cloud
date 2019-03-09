b8ee37a90c21fcfefe49496a351985655eaa4f6c
Takuya HARA <46240835+takuyahara@users.noreply.github.com>
2019-03-09 22:54:57 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import SEO from "../components/seo";
 import AxlMetronome from '../components/AxlMetronome/AxlMetronome';
 import bugsnag from '@bugsnag/js';
 import bugsnagReact from '@bugsnag/plugin-react';
+import { version as appVersion } from '../../package.json';
 
 const NODE_CONFIG = JSON.parse(process.env.NODE_CONFIG!);
 const defaultConfig = NODE_CONFIG.AxlMetronome;
@@ -33,7 +34,7 @@ const IndexPage = () => (
 if (process.env.NODE_ENV === 'production') {
   const bugsnagClient = bugsnag({
     apiKey: bsConfig.API,
-    appVersion: process.env.npm_package_version,
+    appVersion,
   });
   bugsnagClient.use(bugsnagReact, React);
 }
