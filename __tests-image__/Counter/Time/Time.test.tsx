425590b6051670fdb98b42d1b309c04793ebb0cf
takuyahara <h.taku86@gmail.com>
2019-04-02 00:12:40 +0900

    Time

--- __tests-image__/Counter/Time/Time.test.tsx
+++ __tests-image__/Counter/Time/Time.test.tsx
@@ -1,8 +1,13 @@
+import puppeteer from 'puppeteer';
 import devices from 'puppeteer/DeviceDescriptors';
 const iPhone = devices[`iPhone 6`];
 
+let browser: puppeteer.Browser;
+let page: puppeteer.Page;
 describe(`Time`, () => {
   beforeEach(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
     await page.emulate(iPhone);
     await page.goto(`http://localhost:6006/iframe.html?id=counter--time`);
   });
